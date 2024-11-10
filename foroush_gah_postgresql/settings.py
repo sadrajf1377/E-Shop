9f053b6fce1ec6dbb3dbb482420c32e380b60b68
sadrajf1377 <150929173+sadrajf1377@users.noreply.github.com>
2024-11-10 12:43:37 -0600

    settings changed

--- foroush_gah_postgresql/settings.py
+++ foroush_gah_postgresql/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v-&z_c=2g-v!lhs13)6^=isk7&a5@*oyof(&_lefeq&(9yuao4'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
