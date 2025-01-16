28c3f6ae34e2740d3a3fa2d41450b9676eede08a
sadrajf1377 <150929173+sadrajf1377@users.noreply.github.com>
2025-01-16 07:59:01 -0600

    Update settings.py

--- foroush_gah_postgresql/settings.py
+++ foroush_gah_postgresql/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.134']
+ALLOWED_HOSTS = []
 
 
 # Application definition
